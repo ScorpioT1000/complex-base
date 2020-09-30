63250bed93e9648c133b985f55e894424cdddc33
ScorpioT1000 <ScorpioT1000@yandex.ru>
2020-09-30 17:12:54 +0300

    Update ComplexTest.php
    remove unnecessary  code from tests

--- tests/ComplexTest.php
+++ tests/ComplexTest.php
@@ -47,15 +47,5 @@ final class ComplexTest extends TestCase
             $caughtDivByZero = true;
         }
         $this->assertSame($caughtDivByZero, true);
-        
-        $stack = [];
-        $this->assertSame(0, count($stack));
-
-        array_push($stack, 'foo');
-        $this->assertSame('foo', $stack[count($stack)-1]);
-        $this->assertSame(1, count($stack));
-
-        $this->assertSame('foo', array_pop($stack));
-        $this->assertSame(0, count($stack));
     }
 }
